c96c16191bb86e328fd652495d5da6b05b6d76bf
bdupertu <baptiste.dupertuis@epfl.ch>
2025-01-06 15:06:36 +0100

    add V2 to do-it.sh test

--- pcm_lb_project/tspcc/do-it.sh
+++ pcm_lb_project/tspcc/do-it.sh
@@ -39,7 +39,7 @@ if [ "$run_testatom" == "y" ]; then
 fi
 
 # ask if they want to run tspcc and put in variable
-read -p "Do you want to run tspcc? (y/N): " run_tspcc
+read -p "Do you want to run tspcc (V1)? (y/N): " run_tspcc
 
 # to lowecase
 run_tspcc=$(echo $run_tspcc | tr '[:upper:]' '[:lower:]')
@@ -47,10 +47,32 @@ run_tspcc=$(echo $run_tspcc | tr '[:upper:]' '[:lower:]')
 # compile tspcc
 make tspcc
 
-# clear screen
-clear
-
 # run tspcc
 if [ "$run_tspcc" == "y" ]; then
-    ./tspcc dj38.tsp
+    read -p "Number of threads? (10): " number_threads
+    number_threads=$(echo "$number_threads" | tr '[:upper:]' '[:lower:]')
+    number_threads=${number_threads:-10}
+    clear
+    echo "Running tspcc with $number_threads threads..."
+    ./tspcc dj38.tsp $number_threads
+fi
+
+# ask if they want to run tspcc and put in variable
+read -p "Do you want to run tspcc (V2)? (y/N): " run_tspcc2
+
+# to lowecase
+run_tspcc2=$(echo $run_tspcc2 | tr '[:upper:]' '[:lower:]')
+
+# compile tspcc
+make clean
+make tspcc CPPFLAGS=-DADVANCE_END_CONDITION
+
+# run tspcc
+if [ "$run_tspcc2" == "y" ]; then
+    read -p "Number of threads? (10): " number_threads
+    number_threads=$(echo "$number_threads" | tr '[:upper:]' '[:lower:]')
+    number_threads=${number_threads:-10}
+    clear
+    echo "Running tspcc with $number_threads threads..."
+    ./tspcc dj38.tsp $number_threads
 fi
